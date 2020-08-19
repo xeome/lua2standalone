c28f0023be8d056e1f59308d21a60c82a744e5a0
xeome <haxorexo@gmail.com>
2020-08-19 21:09:00 +0300

    convert function
    the function to convert precompiled bytecode into hex

--- main.lua
+++ main.lua
@@ -1,2 +1,12 @@
---test
---commit test
+local input = io.open("input.lua","rb"):read("*all")
+
+local precompiled = string.dump(loadstring(input))
+
+local function convert(s)
+	local converted = {}
+	return s:gsub(".",function(a)
+		local dec = a:byte()
+		table.insert(converted,string.format("%x",dec))
+	end)
+	return converted
+end
